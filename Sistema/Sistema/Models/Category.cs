d6871e941a9e7049da1d309f315b939cb00f874a
royrvm <46869929+royrvm@users.noreply.github.com>
2020-03-31 21:47:12 -0500

    The basic protect was created with the category controller

---
+++ Sistema/Sistema/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength =3,ErrorMessage ="The name must have from 3 to 50 characters")]
+        public string Name { get; set; }
+
+        [StringLength(256,ErrorMessage ="The description must not exceed 256 characters")]
+        [Display(Name="Descripción")]
+        public string Description { get; set; }
+
+        public Boolean State { get; set; }
+    }
+}
